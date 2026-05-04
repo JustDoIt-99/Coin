48e2fa18be9158e0dcabf30fd0d1efa32bc01c4b
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-04 17:50:39 +0900

    [#3] feat: 마켓 사이드바 구현 및 정렬/필터 상태 관리 추가

---
+++ src/components/market/MarketSidebar.tsx
@@ -0,0 +1,97 @@
+import styled from "@emotion/styled";
+import type {Market, Ticker} from "../../api/api.ts";
+import MarketRow from "./MarketRow.tsx";
+import type {MarketTab} from "../../page/MarketPage.tsx";
+import MarketTabs from "./MarketTabs.tsx";
+import MarketHeaderRow from "./MarketHeaderRow.tsx";
+import {useState} from "react";
+
+const Container = styled.aside`
+    width:420px;
+    height: 100vh;
+    background: #fff;
+    border-left:  1px solid #dfe3ea;
+    overflow-y: auto;
+`;
+
+interface Props {
+    markets: Market[];
+    activeTab: MarketTab;
+    setActiveTab: (tab: MarketTab) => void;
+    tickers: Record<string, Ticker>;
+}
+
+export type NameType = "korean" | "english";
+export type SortedKey = "price" | "change" | "volume" | null;
+export type SortOrder = "asc" | "desc" | null;
+
+function MarketSidebar({markets, activeTab, setActiveTab, tickers} : Props) {
+
+    const btcPrice = tickers["KRW-BTC"]?.trade_price;
+    const usdtPrice = tickers["KRW-USDT"]?.trade_price;
+
+    const [sortKey, setSortKey] = useState<SortedKey>(null);
+    const [sortOrder, setSortOrder] = useState<SortOrder>(null);
+
+    const [nameType, setNameType] = useState<NameType>("korean");
+
+    function handleSort(key: SortedKey) {
+        if (sortKey !== key) {
+            setSortKey(key);
+            setSortOrder("desc");
+            return;
+        }
+
+        if (sortOrder === "desc") {
+            setSortOrder("asc");
+        } else if (sortOrder === "asc") {
+            setSortKey(null);
+            setSortOrder(null);
+        } else {
+            setSortOrder("desc");
+        }
+    }
+
+    const sortedMarkets = [...markets].sort((a,b) => {
+        if (!sortKey || !sortOrder) return 0;
+
+        const aTicker = tickers[a.market];
+        const bTicker = tickers[b.market];
+
+        const getValue = (t?: Ticker) => {
+            if (!t) return 0;
+            if (sortKey === "price") return t.trade_price;
+            if (sortKey === "change") return t.signed_change_rate;
+            if (sortKey === "volume") return t.acc_trade_price_24h;
+            return 0;
+        };
+
+        const diff = getValue(aTicker) - getValue(bTicker);
+        return sortOrder === "asc" ? diff : -diff;
+    })
+
+    return (
+        <Container>
+            <MarketTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <MarketHeaderRow
+                nameType={nameType}
+                setNameType={setNameType}
+                sortKey={sortKey}
+                sortOrder={sortOrder}
+                handleSort={handleSort}
+            />
+            {sortedMarkets?.map((market) => (
+                <MarketRow
+                    key={market.market}
+                    market={market}
+                    ticker={tickers[market.market]}
+                    btcPrice={btcPrice}
+                    usdtPrice={usdtPrice}
+                    nameType={nameType}
+                />
+            ))}
+        </Container>
+    )
+}
+
+export default MarketSidebar;
