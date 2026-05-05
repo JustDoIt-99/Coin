0c4cdde5be8232a001b83fe67eca0217ce8b8d9a
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-05 14:42:20 +0900

    [#3] chore: 파일 이동

--- src/components/market/sidebar/MarketSidebar.tsx
+++ src/components/market/sidebar/MarketSidebar.tsx
@@ -1,17 +1,24 @@
 import styled from "@emotion/styled";
-import type {Market, Ticker} from "../../api/api.ts";
+import type {Market, Ticker} from "../../../api/api.ts";
 import MarketRow from "./MarketRow.tsx";
-import type {MarketTab} from "../../page/MarketPage.tsx";
+import type {MarketTab} from "../../../page/MarketPage.tsx";
 import MarketTabs from "./MarketTabs.tsx";
 import MarketHeaderRow from "./MarketHeaderRow.tsx";
-import {useState} from "react";
+import {useDeferredValue, useState} from "react";
+import MarketSearch from "./MarketSearch.tsx";
 
 const Container = styled.aside`
-    width:420px;
+    width: 420px;
     height: 100vh;
     background: #fff;
-    border-left:  1px solid #dfe3ea;
-    overflow-y: auto;
+    border-left: 1px solid #dfe3ea;
+    display: flex;
+    flex-direction: column;
+`;
+
+const RowList = styled.div`
+  flex: 1;
+  overflow-y: auto;
 `;
 
 interface Props {
@@ -34,6 +41,8 @@ function MarketSidebar({markets, activeTab, setActiveTab, tickers} : Props) {
     const [sortOrder, setSortOrder] = useState<SortOrder>(null);
 
     const [nameType, setNameType] = useState<NameType>("korean");
+    const [search, setSearch] = useState<string>("");
+    const deferredSearch = useDeferredValue(search);
 
     function handleSort(key: SortedKey) {
         if (sortKey !== key) {
@@ -52,7 +61,17 @@ function MarketSidebar({markets, activeTab, setActiveTab, tickers} : Props) {
         }
     }
 
-    const sortedMarkets = [...markets].sort((a,b) => {
+    const filteredMarkets = markets.filter((market) => {
+        const keyword = deferredSearch.toLowerCase();
+
+        return (
+            market.korean_name.toLowerCase().includes(keyword) ||
+            market.english_name.toLowerCase().includes(keyword) ||
+            market.market.toLowerCase().includes(keyword)
+        );
+    })
+
+    const sortedMarkets = [...filteredMarkets].sort((a,b) => {
         if (!sortKey || !sortOrder) return 0;
 
         const aTicker = tickers[a.market];
@@ -72,6 +91,7 @@ function MarketSidebar({markets, activeTab, setActiveTab, tickers} : Props) {
 
     return (
         <Container>
+            <MarketSearch onSearch={setSearch}/>
             <MarketTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
             <MarketHeaderRow
                 nameType={nameType}
@@ -80,16 +100,18 @@ function MarketSidebar({markets, activeTab, setActiveTab, tickers} : Props) {
                 sortOrder={sortOrder}
                 handleSort={handleSort}
             />
-            {sortedMarkets?.map((market) => (
-                <MarketRow
-                    key={market.market}
-                    market={market}
-                    ticker={tickers[market.market]}
-                    btcPrice={btcPrice}
-                    usdtPrice={usdtPrice}
-                    nameType={nameType}
-                />
-            ))}
+            <RowList>
+                {sortedMarkets?.map((market) => (
+                    <MarketRow
+                        key={market.market}
+                        market={market}
+                        ticker={tickers[market.market]}
+                        btcPrice={btcPrice}
+                        usdtPrice={usdtPrice}
+                        nameType={nameType}
+                    />
+                ))}
+            </RowList>
         </Container>
     )
 }
--- src/components/market/sidebar/MarketTabs.tsx
+++ src/components/market/sidebar/MarketTabs.tsx
@@ -1,21 +1,22 @@
-import type {MarketTab} from "../../page/MarketPage.tsx";
+import type {MarketTab} from "../../../page/MarketPage.tsx";
 import styled from "@emotion/styled";
 
 const Tabs = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  height: 48px;
-  border-bottom: 1px solid #dfe3ea;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 48px;
+    border-right: 1px solid #dfe3ea;
+    border-bottom: 1px solid #dfe3ea;
 `;
 
 const TabButton = styled.button<{ isActive: boolean }>`
-  border: none;
-  background: #fff;
-  cursor: pointer;
-  font-weight: 700;
-  color: ${({ isActive }) => (isActive ? "#0062df" : "#333")};
-  border-bottom: ${({ isActive }) =>
-          isActive ? "3px solid #0062df" : "3px solid transparent"};
+    border: none;
+    background: #fff;
+    cursor: pointer;
+    font-weight: 700;
+    color: ${({isActive}) => (isActive ? "#0062df" : "#333")};
+    border-bottom: ${({isActive}) =>
+            isActive ? "3px solid #0062df" : "3px solid transparent"};
 `;
 
 interface Props {
