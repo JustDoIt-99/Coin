74eb2d2a669bed01c8885c2e8245e52ebf9ef1cd
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-05 22:10:09 +0900

    [#5] refactor: style 코드 분리

--- src/components/market/sidebar/MarketSearch/MarketSearch.tsx
+++ src/components/market/sidebar/MarketSearch/MarketSearch.tsx
@@ -1,49 +1,6 @@
-import styled from "@emotion/styled";
 import {Search, Settings} from "lucide-react";
 import {useEffect, useState} from "react";
-
-const SearchBar = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 48px 48px;
-    height: 52px;
-    border-bottom: 1px solid #dfe3ea;
-    border-top: 1px solid #dfe3ea;
-    background: #fff;
-`;
-
-const Input = styled.input`
-    border: none;
-    padding: 0 16px;
-    font-size: 14px;
-    color: #333;
-    outline: none;
-
-    &::placeholder {
-        color: #9aa1ad;
-    }
-`;
-
-const SearchButton = styled.button`
-    border: none;
-    background: #fff;
-    color: #0062df;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-`;
-
-const SettingButton = styled.button`
-    border: none;
-    border-left: 1px solid #dfe3ea;
-    border-right: 1px solid #dfe3ea;
-    background: #fff;
-    color: #999;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-`;
+import {Input, SearchBar, SearchButton, SettingButton} from "./MarketSearch.styles.ts";
 
 interface Props {
     onSearch: (value: string) => void;
