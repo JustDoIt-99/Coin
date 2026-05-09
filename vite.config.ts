dd0ad1c90bc53a70762138d0aa011e8136e86dd7
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-09 16:31:21 +0900

    [#11] chore: path alias 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
             "@components": path.resolve(__dirname, "./src/components"),
             "@hooks": path.resolve(__dirname, "./src/hooks"),
             "@constants": path.resolve(__dirname, "./src/constants"),
+            "@pages": path.resolve(__dirname, "./src/pages")
         }
     }
 })
