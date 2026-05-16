dadf1058651b2ea7ac50c5302e473f7363e8b16b
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-16 17:04:11 +0900

    [#17] refactor: API.CANDLES /candles/minutes/ url에서 / 제거

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -3,5 +3,5 @@ export const BASE_URL = "https://api.upbit.com/v1";
 export const API = {
     MARKETS: `${BASE_URL}/market/all`,
     TICKERS: `${BASE_URL}/ticker`,
-    CANDLES: `${BASE_URL}/candles/minutes/`
+    CANDLES: `${BASE_URL}/candles/minutes`
 }
