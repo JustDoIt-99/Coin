76c7ff5dca74a8e1f763f073b4425f592d564390
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-18 22:44:13 +0900

    [#20] style: 호가창 시장 거래 정보 style 코드 구현

---
+++ src/components/market/orderbook/MarketInfoPanel/MarketInfoPanel.styles.ts
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.aside`
+  width: 240px;
+  padding: 0px 18px;
+  background: #fff;
+  font-size: 14px;
+  color: #222;
+`;
+
+export const InfoGroup = styled.div`
+  padding: 3px 0;
+  border-bottom: 1px solid #e5e8ee;
+
+  &:first-of-type {
+    padding-top: 0;
+  }
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+`;
+
+export const InfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 30px;
+
+  span {
+    color: #666;
+    font-weight: 600;
+  }
+`;
+
+export const Value = styled.strong`
+  color: #222;
+  font-size: 15px;
+  font-weight: 700;
+  text-align: right;
+`;
+
+export const Unit = styled.small`
+  margin-left: 4px;
+  color: #999;
+  font-size: 13px;
+  font-weight: 600;
+`;
+
+export const DateText = styled.div`
+    text-align: right;
+    color: #999;
+    font-size: 13px;
+    line-height: 20px;
+
+    &.red {
+        color: #d64348;
+    }
+
+    &.blue {
+        color: #126ee2;
+    }
+`;
+
+export const RedValue = styled(Value)`
+  color: #d64348;
+`;
+
+export const BlueValue = styled(Value)`
+  color: #126ee2;
+`;
