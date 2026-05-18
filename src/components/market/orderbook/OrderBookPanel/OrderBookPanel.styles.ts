e4b7c8a5164dfb86e7282dc6b6c0441a4fd29779
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-19 08:21:06 +0900

    [#20] style: 호가 패널 및 리스트 레이아웃 스타일 정의

---
+++ src/components/market/orderbook/OrderBookPanel/OrderBookPanel.styles.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const Panel = styled.div`
+    position: relative;
+    display: grid;
+    width: 350px;
+    background: #fff;
+    overflow-y: auto;
+    overflow-x: hidden;
+`;
+
+export const OrderBookList = styled.div`
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+`;
