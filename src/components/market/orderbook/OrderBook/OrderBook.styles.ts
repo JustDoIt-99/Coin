2b1caa9f1d631c3c4c324be6bea3f2192cd2910c
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-19 08:20:31 +0900

    [#20] style: 호가창 레이아웃 및 헤더 스타일 정의

---
+++ src/components/market/orderbook/OrderBook/OrderBook.styles.ts
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const OrderBookContainer = styled.div`
+    height: 700px;
+    width: 590px;
+    background-color: #fff;
+    overflow-y: auto;
+    overflow-x: hidden;
+`;
+
+export const Section = styled.div`
+    display: grid;
+    grid-template-columns: 340px 250px;
+`;
+
+export const MarketInfoWrapper = styled.div`
+    align-self: end;
+`;
+
+export const OrderBookHeader = styled.div`
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    background: #fff;
+    border-bottom: 1px solid #e5e8ee;
+`;
+
+export const ColumnHeader = styled.div`
+    display: grid;
+    grid-template-columns: 130px 140px 70px 230px;
+    height: 32px;
+    background: #f8f9fb;
+
+    span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 13px;
+        font-weight: 600;
+        color: #666;
+    }
+`;
