e530c56021cd08210d31886e45bb1a82519a7104
JustDoIt-99 <sangyunpark99@gmail.com>
2026-06-04 21:57:14 +0900

    [#51] feat: CORS URL yml 파일에서 관리

--- backend/src/main/java/com/sangyunpark/backend/common/config/WebConfig.java
+++ backend/src/main/java/com/sangyunpark/backend/common/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.sangyunpark.backend.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins[0]}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(allowedOrigin)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowedMethods("*")
--- backend/src/main/java/com/sangyunpark/backend/market/config/WebSocketConfig.java
+++ backend/src/main/java/com/sangyunpark/backend/market/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.sangyunpark.backend.market.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${cors.allowed-origins[0]}")
+    private String allowedOrigin;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173");
+                .setAllowedOrigins(allowedOrigin);
     }
 
     @Override
